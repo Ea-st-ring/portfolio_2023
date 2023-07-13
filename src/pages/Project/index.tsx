88a62c8aaee55f8623e9e0e041743cebfedc1503
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2023-07-13 22:22:04 +0900

    [feat] 배경 및 타이틀 애니메이션 구현

--- src/pages/Project/index.tsx
+++ src/pages/Project/index.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import background_img from './assets/project_bg.png';
+import styled from 'styled-components';
+import Title from './Title';
 
 const Project = () => {
     return (
-        <div>
-            나는 프로젝트임 
-        </div>
+        <Background>
+        <Title text="Hyun's Projects"></Title>
+        <ProjectWrapper>
+        </ProjectWrapper>
+        </Background>
     );
 };
+
+const Background = styled.div`
+    background-image: url(${background_img});
+    background-size: cover repeat;
+    width: 1440px;
+    height: 1024px;
+    position: absolute;
+    top: 0;
+    left: 0;
+`
+
+const ProjectWrapper = styled.div`
+    width: 1078px;
+    height: 696px;
+    margin : 0 auto;
+    flex-shrink: 0;
+    border-radius: 15px;
+    background: rgba(255, 255, 255, 0.85);
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    backdrop-filter: blur(1px);
+`
 
 export default Project;
